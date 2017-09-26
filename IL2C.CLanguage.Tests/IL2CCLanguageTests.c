a2b369906b15d748cf4176256cc76b58fd00c315
Kouji Matsui <k@kekyo.net>
2017-09-26 23:36:57 +0900

    #6-14

--- IL2C.CLanguage.Tests/IL2CCLanguageTests.c
+++ IL2C.CLanguage.Tests/IL2CCLanguageTests.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+
 int64_t main(void)
 {
     int64_t local0;
@@ -6,9 +7,22 @@ int64_t main(void)
     int64_t local2;
     int64_t local3;
 
-    local0 = 1LL;
-    local1 = 2LL;
-    local2 = local1 + local0;
-    local3 = local2;
-    return local3;
+    int32_t __stack0_int32_t;
+    int64_t __stack0_int64_t;
+    int64_t __stack1_int64_t;
+
+    __stack0_int32_t = 1;
+    __stack0_int64_t = (int64_t)__stack0_int32_t;
+    local0 = __stack0_int64_t;
+    __stack0_int32_t = 2;
+    __stack0_int64_t = (int64_t)__stack0_int32_t;
+    local1 = __stack0_int64_t;
+    __stack0_int64_t = local0;
+    __stack1_int64_t = local1;
+    __stack0_int64_t = __stack0_int64_t + __stack1_int64_t;
+    local2 = __stack0_int64_t;
+    __stack0_int64_t = local2;
+    local3 = __stack0_int64_t;
+    __stack0_int64_t = local3;
+    return __stack0_int64_t;
 }

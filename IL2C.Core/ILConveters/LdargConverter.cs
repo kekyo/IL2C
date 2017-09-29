bfa002c131462cb7f70d49bcd1cfd758ebfe3fc3
Kouji Matsui <k@kekyo.net>
2017-09-29 22:56:37 +0900

    Fixed ldarg target argumenttype handling.

--- IL2C.Core/ILConveters/LdargConverter.cs
+++ IL2C.Core/ILConveters/LdargConverter.cs
@@ -2,15 +2,47 @@
 
 namespace IL2C.ILConveters
 {
+    internal static class LdargConverterUtilities
+    {
+        public static string Apply(int parameterIndex, DecodeContext context)
+        {
+            var parameter = context.Parameters[parameterIndex];
+            var targetType = parameter.ParameterType;
+
+            if (targetType == typeof(bool))
+            {
+                var symbolName = context.PushStack(typeof(int));
+                return string.Format(
+                    "{0} = {1} ? 1 : 0",
+                    symbolName,
+                    parameter.Name);
+            }
+            else
+            {
+                if ((targetType == typeof(byte))
+                    || (targetType == typeof(sbyte))
+                    || (targetType == typeof(short))
+                    || (targetType == typeof(ushort)))
+                {
+                    targetType = typeof(int);
+                }
+
+                var symbolName = context.PushStack(targetType);
+                return string.Format(
+                    "{0} = {1}",
+                    symbolName,
+                    parameter.Name);
+            }
+        }
+    }
+
     internal sealed class Ldarg_0Converter : InlineNoneConverter
     {
         public override OpCode OpCode => OpCodes.Ldarg_0;
 
         public override string Apply(DecodeContext context)
         {
-            var parameter = context.Parameters[0];
-            var symbolName = context.PushStack(parameter.ParameterType);
-            return string.Format("{0} = {1}", symbolName, parameter.Name);
+            return LdargConverterUtilities.Apply(0, context);
         }
     }
 
@@ -20,9 +52,7 @@ namespace IL2C.ILConveters
 
         public override string Apply(DecodeContext context)
         {
-            var parameter = context.Parameters[1];
-            var symbolName = context.PushStack(parameter.ParameterType);
-            return string.Format("{0} = {1}", symbolName, parameter.Name);
+            return LdargConverterUtilities.Apply(1, context);
         }
     }
 }

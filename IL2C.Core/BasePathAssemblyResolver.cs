73e587cb82a71959fca1864ef3664877082600dc
Kouji Matsui <k@kekyo.net>
2017-11-29 19:21:11 +0900

    Temporary disabled tests on netcoreapp, turn to net45.

--- IL2C.Core/BasePathAssemblyResolver.cs
+++ IL2C.Core/BasePathAssemblyResolver.cs
@@ -8,11 +8,28 @@ using Mono.Cecil;
 namespace IL2C
 {
 #if true
-    internal sealed class BasePathAssemblyResolver : DefaultAssemblyResolver
+    internal sealed class BasePathAssemblyResolver : IAssemblyResolver
     {
+        private readonly DefaultAssemblyResolver resolver = new DefaultAssemblyResolver();
+
         public BasePathAssemblyResolver(string basePath)
         {
-            base.AddSearchDirectory(basePath);
+            resolver.AddSearchDirectory(basePath);
+        }
+
+        public void Dispose()
+        {
+            resolver.Dispose();
+        }
+
+        public AssemblyDefinition Resolve(AssemblyNameReference name)
+        {
+            return resolver.Resolve(name);
+        }
+
+        public AssemblyDefinition Resolve(AssemblyNameReference name, ReaderParameters parameters)
+        {
+            return resolver.Resolve(name, parameters);
         }
     }
 #else

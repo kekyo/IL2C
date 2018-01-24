c06e8230edb4bdcc60cd780abd4d412d8100c6fd
Kouji Matsui <k@kekyo.net>
2018-01-24 23:52:00 +0900

    Implementing polish notation parser (WIP)

--- tests/UEFI/UEFI.Code.Tests/PolishNotationTests.cs
+++ tests/UEFI/UEFI.Code.Tests/PolishNotationTests.cs
@@ -6,31 +6,146 @@ namespace UEFI.Code
     [TestFixture]
     public class PolishNotationTests
     {
+        #region SkipWhiteSpace
+        [Test]
+        public void SkipWhiteSpaceFromIndex0Test()
+        {
+            var index = PolishNotation.SkipWhiteSpace("   ", 0);
+
+            Assert.AreEqual(3, index);
+        }
+
+        [Test]
+        public void SkipWhiteSpaceTest()
+        {
+            var index = PolishNotation.SkipWhiteSpace("     ", 2);
+
+            Assert.AreEqual(5, index);
+        }
+        #endregion
+
+        #region ParseOperator
         [Test]
         public void OperatorAddParseFromIndex0Test()
         {
-            var expr = PolishNotation.ParseOperator("+", 0);
+            var oper = PolishNotation.ParseOperator("+", 0);
 
-            Assert.AreEqual('+', expr.Operator);
-            Assert.AreEqual(1, expr.NextIndex);
+            Assert.AreEqual('+', oper.Operator);
+            Assert.AreEqual(1, oper.NextIndex);
         }
 
         [Test]
         public void OperatorAddParseWithTrailingSpaceTest()
         {
-            var expr = PolishNotation.ParseOperator("+   ", 0);
+            var oper = PolishNotation.ParseOperator("+   ", 0);
 
-            Assert.AreEqual('+', expr.Operator);
-            Assert.AreEqual(4, expr.NextIndex);
+            Assert.AreEqual('+', oper.Operator);
+            Assert.AreEqual(4, oper.NextIndex);
         }
 
         [Test]
         public void OperatorAddParseBeginningPartialIndexTest()
         {
-            var expr = PolishNotation.ParseOperator("  +  ", 2);
+            var oper = PolishNotation.ParseOperator("  +  ", 2);
 
-            Assert.AreEqual('+', expr.Operator);
-            Assert.AreEqual(5, expr.NextIndex);
+            Assert.AreEqual('+', oper.Operator);
+            Assert.AreEqual(5, oper.NextIndex);
         }
+        #endregion
+
+        #region ParseNumeric
+        [Test]
+        public void NumericParseFromIndex0Test()
+        {
+            var numeric = PolishNotation.ParseNumeric("123", 0);
+
+            Assert.AreEqual(123, numeric.Numeric);
+            Assert.AreEqual(3, numeric.NextIndex);
+        }
+
+        [Test]
+        public void NumericParseWithTrailingSpaceTest()
+        {
+            var numeric = PolishNotation.ParseNumeric("123   ", 0);
+
+            Assert.AreEqual(123, numeric.Numeric);
+            Assert.AreEqual(6, numeric.NextIndex);
+        }
+
+        [Test]
+        public void NumericParseBeginningPartialIndexTest()
+        {
+            var numeric = PolishNotation.ParseNumeric("  123  ", 2);
+
+            Assert.AreEqual(123, numeric.Numeric);
+            Assert.AreEqual(7, numeric.NextIndex);
+        }
+
+        [Test]
+        public void MaxNumericParseTest()
+        {
+            var valueString = int.MaxValue.ToString();
+            var numeric = PolishNotation.ParseNumeric(valueString, 0);
+
+            Assert.AreEqual(int.MaxValue, numeric.Numeric);
+            Assert.AreEqual(valueString.Length, numeric.NextIndex);
+        }
+
+        [Test]
+        public void MinNumericParseTest()
+        {
+            var valueString = int.MinValue.ToString();
+            var numeric = PolishNotation.ParseNumeric(valueString, 0);
+
+            Assert.AreEqual(int.MinValue, numeric.Numeric);
+            Assert.AreEqual(valueString.Length, numeric.NextIndex);
+        }
+        #endregion
+
+        #region ParseExpression
+        [Test]
+        public void ExpressionParseFromIndex0Test()
+        {
+            var expr = PolishNotation.ParseExpression("+ 123 456", 0);
+
+            Assert.AreEqual('+', expr.Operator.Operator);
+            Assert.AreEqual(123, expr.LeftNumeric.Numeric);
+            Assert.AreEqual(456, expr.RightNumeric.Numeric);
+            Assert.AreEqual(9, expr.NextIndex);
+        }
+
+        [Test]
+        public void ExpressionParseWithTrailingSpaceTest()
+        {
+            var expr = PolishNotation.ParseExpression("+ 123 456   ", 0);
+            
+            Assert.AreEqual('+', expr.Operator.Operator);
+            Assert.AreEqual(123, expr.LeftNumeric.Numeric);
+            Assert.AreEqual(456, expr.RightNumeric.Numeric);
+            Assert.AreEqual(12, expr.NextIndex);
+        }
+
+        [Test]
+        public void ExpressionParseBeginningPartialIndexTest()
+        {
+            var expr = PolishNotation.ParseExpression("   + 123 456   ", 3);
+
+            Assert.AreEqual('+', expr.Operator.Operator);
+            Assert.AreEqual(123, expr.LeftNumeric.Numeric);
+            Assert.AreEqual(456, expr.RightNumeric.Numeric);
+            Assert.AreEqual(15, expr.NextIndex);
+        }
+
+        [Test]
+        public void ExpressionParseSparseTest()
+        {
+            var expr = PolishNotation.ParseExpression("+   123  456   ", 0);
+
+            Assert.AreEqual('+', expr.Operator.Operator);
+            Assert.AreEqual(123, expr.LeftNumeric.Numeric);
+            Assert.AreEqual(456, expr.RightNumeric.Numeric);
+            Assert.AreEqual(15, expr.NextIndex);
+        }
+        #endregion
     }
 }
--- tests/UEFI/UEFI.Code/PolishNotation.cs
+++ tests/UEFI/UEFI.Code/PolishNotation.cs
@@ -6,27 +6,59 @@ using System.Text;
 
 namespace UEFI.Code
 {
-    public struct OperatorResult
+    public abstract class PolishNotationNode
     {
-        public readonly char Operator;
         public readonly int NextIndex;
 
-        public OperatorResult(char oper, int index)
+        protected PolishNotationNode(int nextIndex)
         {
-            this.Operator = oper;
-            this.NextIndex = index;
+            this.NextIndex = nextIndex;
         }
     }
 
-    public struct NumericResult
+    public class OperatorNode : PolishNotationNode
+    {
+        public readonly char Operator;
+
+        public OperatorNode(char oper, int index) : base(index)
+        {
+            this.Operator = oper;
+        }
+    }
+    
+    public class NumericNode : PolishNotationNode
     {
         public readonly int Numeric;
-        public readonly int NextIndex;
 
-        public NumericResult(int numeric, int index)
+        public NumericNode(int numeric, int index) : base(index)
         {
             this.Numeric = numeric;
-            this.NextIndex = index;
+        }
+    }
+
+    public class ExpressionNode : PolishNotationNode
+    {
+        public readonly OperatorNode Operator;
+        public readonly NumericNode LeftNumeric;
+        public readonly NumericNode RightNumeric;
+
+        public ExpressionNode(OperatorNode oper, NumericNode leftNumeric, NumericNode rightNumeric, int index) : base(index)
+        {
+            this.Operator = oper;
+            this.LeftNumeric = leftNumeric;
+            this.RightNumeric = rightNumeric;
+        }
+    }
+
+    public class CombinedExpressionNode : PolishNotationNode
+    {
+        public readonly ExpressionNode FirstExpression;
+        public readonly ExpressionNode SecondExpression;
+
+        public CombinedExpressionNode(ExpressionNode firstExpression, ExpressionNode secondExpression, int index) : base(index)
+        {
+            this.FirstExpression = firstExpression;
+            this.SecondExpression = secondExpression;
         }
     }
 
@@ -37,44 +69,99 @@ namespace UEFI.Code
             var index = startIndex;
             while (index < line.Length)
             {
-                var ch = line[index++];
-                if ((ch != ' ') && (ch != '\t'))
-                {
-                    break;
-                }
-            }
-
-            return index;
-        }
-
-        internal static OperatorResult ParseOperator(string line, int startIndex)
-        {
-            var index = startIndex;
-            var ch = line[index++];
-
-            index = SkipWhiteSpace(line, index);
-            return new OperatorResult(ch, index);
-        }
-
-        internal static NumericResult ParseNumeric(string line, int startIndex)
-        {
-            var index = startIndex;
-            while (index < line.Length)
-            {
                 var ch = line[index];
-                if ((ch < '0') || (ch > '9'))
+                if ((ch != ' ') && (ch != '\t'))
                 {
                     break;
                 }
                 index++;
             }
 
+            return index;
+        }
+
+        internal static OperatorNode ParseOperator(string line, int startIndex)
+        {
+            if (startIndex >= line.Length)
+            {
+                return null;
+            }
+
+            var index = startIndex;
+            var ch = line[index];
+            if ((ch != '+') && (ch != '-') && (ch != '*') && (ch != '/'))
+            {
+                return null;
+            }
+            index++;
+
+            index = SkipWhiteSpace(line, index);
+            return new OperatorNode(ch, index);
+        }
+
+        internal static NumericNode ParseNumeric(string line, int startIndex)
+        {
+            if (startIndex >= line.Length)
+            {
+                return null;
+            }
+
+            var index = startIndex;
+            while (index < line.Length)
+            {
+                var ch = line[index];
+                if ((((ch >= '0') && (ch <= '9')) || (ch == '-')) == false)
+                {
+                    break;
+                }
+                index++;
+            }
+
+            if (index == startIndex)
+            {
+                return null;
+            }
+
             var token = line.Substring(startIndex, index - startIndex);
             var numeric = int.Parse(token);
 
             index = SkipWhiteSpace(line, index);
-            return new NumericResult(numeric, index);
+            return new NumericNode(numeric, index);
         }
+
+        internal static ExpressionNode ParseExpression(string line, int startIndex)
+        {
+            if (startIndex >= line.Length)
+            {
+                return null;
+            }
+
+            var oper = ParseOperator(line, startIndex);
+            if (oper == null)
+            {
+                return null;
+            }
+
+            var numericLeft = ParseNumeric(line, oper.NextIndex);
+            if (numericLeft == null)
+            {
+                return null;
+            }
+
+            var numericRight = ParseNumeric(line, numericLeft.NextIndex);
+            if (numericRight == null)
+            {
+                return null;
+            }
+
+            var index = SkipWhiteSpace(line, numericRight.NextIndex);
+            return new ExpressionNode(oper, numericLeft, numericRight, index);
+        }
+
+        //internal static CombinedExpressionResult ParseCombinedExpression(string line, int startIndex)
+        //{
+            
+        //}
 
         public static void Main()
         {

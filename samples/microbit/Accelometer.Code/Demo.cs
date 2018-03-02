2c7ceb11a13e653da0f9908597c108b0fb36b017
Kouji Matsui <k@kekyo.net>
2018-03-02 22:16:48 +0900

    Implements micro:bit target (WIP)

---
+++ samples/microbit/Accelometer.Code/Demo.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Accelometer
+{
+    public class Demo
+    {
+        [DllImport("Accelometer.h", EntryPoint="accelometer.begin")]
+        private static extern void BeginAccelometer(bool highResolution = true, byte scale = 2);
+
+        [DllImport("Accelometer.h", EntryPoint="accelometer.update")]
+        private static extern byte UpdateAccelometer();
+
+        [DllImport("Accelometer.h", EntryPoint = "accelometer.getX")]
+        private static extern sbyte GetAccelometerX();
+
+        [DllImport("Accelometer.h", EntryPoint = "accelometer.getY")]
+        private static extern sbyte GetAccelometerY();
+
+        [DllImport("Accelometer.h", EntryPoint = "accelometer.getZ")]
+        private static extern sbyte GetAccelometerZ();
+
+        private enum ArrowNames
+        {
+            North = 0,
+            NorthEast = 1,
+            East = 2,
+            SouthEast = 3,
+            South = 4,
+            SouthWest = 5,
+            West = 6,
+            NorthWest = 7
+        }
+
+        [DllImport("Accelometer.h", EntryPoint = "SCREEN.showArrow")]
+        private static extern sbyte ShowArrow(int direction, uint interval = 750);
+
+        private enum IconNames
+        {
+            Heart = 0,
+            SmallHeart = 1,
+            Yes = 2,
+            No = 3,
+            Happy = 4,
+            Sad = 5,
+            Confused = 6,
+            Angry = 7,
+            Asleep = 8,
+            Surprised = 9,
+            Silly = 10,
+            Fabulous = 11,
+            Meh = 12,
+            TShirt = 13,
+            Rollerskate = 14,
+            Duck = 15,
+            House = 16,
+            Tortoise = 17,
+            Butterfly = 18,
+            StickFigure = 19,
+            Ghost = 20,
+            Sword = 21,
+            Giraffe = 22,
+            Skull = 23,
+            Umbrella = 24,
+            Snake = 25,
+            Rabbit = 26,
+            Cow = 27,
+            QuarterNote = 28,
+            EigthNote = 29,
+            Pitchfork = 30,
+            Target = 31,
+            Triangle = 32,
+            LeftTriangle = 33,
+            Chessboard = 34,
+            Diamond = 35,
+            SmallDiamond = 36,
+            Square = 37,
+            SmallSquare = 38,
+            Scissors = 39
+        }
+
+        [DllImport("Accelometer.h", EntryPoint = "SCREEN.showIcon")]
+        private static extern sbyte ShowIcon(int icon, uint interval = 750);
+
+        [DllImport("Accelometer.h", EntryPoint = "delay")]
+        private static extern void Delay(int millis);
+
+        public static void Main()
+        {
+            BeginAccelometer(false, 2);
+
+            while (true)
+            {
+                UpdateAccelometer();
+
+                var x = GetAccelometerX();
+                var y = GetAccelometerY();
+
+                var fn = y > 20;
+                var fs = y < -20;
+                var fw = x > 20;
+                var fe = x < -20;
+
+                if (fn && fe) ShowArrow((int)ArrowNames.NorthEast);
+                else if (fn && fw) ShowArrow((int)ArrowNames.NorthWest);
+                else if (fn) ShowArrow((int)ArrowNames.North);
+                else if (fs && fe) ShowArrow((int)ArrowNames.SouthEast);
+                else if (fs && fw) ShowArrow((int)ArrowNames.SouthWest);
+                else if (fs) ShowArrow((int)ArrowNames.South);
+                else if (fe) ShowArrow((int)ArrowNames.East);
+                else if (fw) ShowArrow((int)ArrowNames.West);
+                else ShowIcon((int)IconNames.Heart);
+
+                Delay(100);
+            }
+        }
+    }
+}
---
+++ samples/microbit/Generated/Accelometer.Code.h
@@ -0,0 +1,46 @@
+﻿#ifndef __MODULE_Accelometer_Code__
+#define __MODULE_Accelometer_Code__
+
+#include <il2c.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//////////////////////////////////////////////////////////////////////////////////
+// Types:
+
+typedef struct Accelometer_Code_Class1 Accelometer_Code_Class1;
+
+////////////////////////////////////////////////////////////
+// Class: Accelometer.Code.Class1
+
+
+extern const __RUNTIME_TYPE__ __Accelometer_Code_Class1_RUNTIME_TYPE__;
+
+//////////////////////////////////////////////////////////////////////////////////
+// Public static fields:
+
+
+//////////////////////////////////////////////////////////////////////////////////
+// Methods:
+
+extern void Accelometer_Code_Class1__ctor(Accelometer_Code_Class1* __this);
+
+typedef const struct
+{
+    intptr_t __reserved0__;
+    intptr_t __reserved1__;
+    intptr_t __reserved2__;
+    System_String* (*ToString)(System_Object* __this);
+    int32_t (*GetHashCode)(System_Object* __this);
+    void (*Finalize)(System_Object* __this);
+    bool (*Equals)(System_Object* __this, System_Object* obj);
+} __Accelometer_Code_Class1_TYPE_DEF_TYPE__;
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif

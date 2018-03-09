b9380dd08a03c8772881b4ddcf022781bca91e9d
Kouji Matsui <k@kekyo.net>
2018-03-08 21:49:01 -0800

    Update circleci settings.

---
+++ pask.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+VERSION=0.3.3
+
+rm -rf artifacts
+mkdir artifacts
+
+echo ""
+echo "///////////////////////////////////////////////"
+echo "// Build IL2C.Core"
+echo ""
+
+dotnet pack --configuration Release --include-symbols --version-suffix ${VERSION} IL2C.Core/IL2C.Core.csproj
+cp IL2C.Core/bin/Release/IL2C.Core.${VERSION}.symbols.nupkg artifacts/IL2C.Core.${VERSION}.nupkg
+
+echo ""
+echo "///////////////////////////////////////////////"
+echo "// Build IL2C.Build"
+echo ""
+
+dotnet pack --configuration Release --include-symbols --version-suffix ${VERSION} IL2C.Tasks/IL2C.Tasks.csproj
+cp IL2C.Tasks/bin/Release/IL2C.Build.${VERSION}.symbols.nupkg artifacts/IL2C.Build.${VERSION}.nupkg
+
+echo ""
+echo "Done."
+echo ""

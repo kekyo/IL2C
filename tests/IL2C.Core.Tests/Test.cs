b4ddd9c3a4268193dcdc586de4fc35a389337814
Kouji Matsui <k@kekyo.net>
2018-10-10 16:31:24 +0900

    Add TestFramework class

--- tests/IL2C.Core.Tests/Test.cs
+++ tests/IL2C.Core.Tests/Test.cs
@@ -12,60 +12,10 @@ namespace IL2C
     [TestFixture]
     public sealed class Test
     {
-        private static readonly Assembly targetAssembly = typeof(Target).Assembly;
-        private static readonly string il2cIncludePath =
-            Path.GetFullPath(
-                Path.Combine(
-                    Path.GetDirectoryName(targetAssembly.Location),
-                    "..",
-                    "..",
-                    "..",
-                    "..",
-                    "..",
-                    "IL2C.Runtime"));
-
-        [Test]
-        public Task TestAsync()
+        [TestCase("ByteMainBody")]
+        public static Task TestTarget(string testName)
         {
-            return TestFramework.ExecuteTestAsync(() => Target.ByteMainBody());
-        }
-
-       // [Test]
-        public async Task SimpleTestAsync()
-        {
-            var translateContext = new TranslateContext(targetAssembly.Location, false);
-
-            var prepared = AssemblyPreparer.Prepare(
-                translateContext, method => method.DeclaringType.FriendlyName == "IL2C.Target");
-
-            var targetMethod =
-                translateContext.Assembly.Modules
-                .First().Types
-                .First(type => type.FriendlyName == "IL2C.Target").DeclaredMethods
-                .First(method => method.Name == "ByteMainBody");
-
-            var tw = new StringWriter();
-            tw.WriteLine("#include <il2c.h>");
-            tw.WriteLine();
-
-            AssemblyWriter.InternalConvertFromMethod(tw, translateContext, prepared, targetMethod, "  ");
-
-            tw.WriteLine();
-            tw.WriteLine("#include <stdio.h>");
-            tw.WriteLine();
-            tw.WriteLine("int main()");
-            tw.WriteLine("{");
-            tw.WriteLine(string.Format("  auto result = {0}();", targetMethod.CLanguageFunctionName));
-            tw.WriteLine(string.Format("  printf(\"%d\", result);"));
-            tw.WriteLine(string.Format("  return 0;"));
-            tw.WriteLine("}");
-
-            var sourceCode = tw.ToString();
-
-            var result = await GccDriver.CompileAndRunAsync(new StringReader(sourceCode), il2cIncludePath);
-            var lines = result.Split(new[] { '\r', '\n' });
-
-            Assert.AreEqual(Target.ByteMainBody().ToString(), lines[0]);
+            return TestFramework.ExecuteTestAsync(typeof(Target), testName);
         }
     }
 }
--- tests/IL2C.Core.Tests/TestFramework.cs
+++ tests/IL2C.Core.Tests/TestFramework.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Threading.Tasks;
 
 using NUnit.Framework;
@@ -21,11 +22,8 @@ namespace IL2C
                     "..",
                     "IL2C.Runtime"));
 
-        public static async Task ExecuteTestAsync<T>(Expression<Func<T>> testFunction)
+        public static async Task ExecuteTestAsync(MethodInfo method)
         {
-            var body = (MethodCallExpression)testFunction.Body;
-            var method = body.Method;
-
             Assert.IsTrue(method.IsPublic && method.IsStatic);
 
             var expected = method.Invoke(null, null);
@@ -41,28 +39,63 @@ namespace IL2C
                 .First(t => t.FriendlyName == method.DeclaringType.FullName).DeclaredMethods
                 .First(m => m.Name == method.Name);
 
-            var tw = new StringWriter();
-            tw.WriteLine("#include <il2c.h>");
-            tw.WriteLine();
+            using (var ms = new MemoryStream())
+            {
+                var tw = new StreamWriter(ms);
 
-            AssemblyWriter.InternalConvertFromMethod(tw, translateContext, prepared, targetMethod, "  ");
+                tw.WriteLine("#include <il2c.h>");
+                tw.WriteLine();
 
-            tw.WriteLine();
-            tw.WriteLine("#include <stdio.h>");
-            tw.WriteLine();
-            tw.WriteLine("int main()");
-            tw.WriteLine("{");
-            tw.WriteLine(string.Format("  auto result = {0}();", targetMethod.CLanguageFunctionName));
-            tw.WriteLine(string.Format("  printf(\"%d\", result);"));
-            tw.WriteLine(string.Format("  return 0;"));
-            tw.WriteLine("}");
+                AssemblyWriter.InternalConvertFromMethod(tw, translateContext, prepared, targetMethod, "  ");
 
-            var sourceCode = tw.ToString();
+                var formatChar =
+                    (targetMethod.ReturnType.IsByteType) ? "%u" :
+                    (targetMethod.ReturnType.IsSByteType) ? "%d" :
+                    (targetMethod.ReturnType.IsUInt16Type) ? "%u" :
+                    (targetMethod.ReturnType.IsInt16Type) ? "%d" :
+                    (targetMethod.ReturnType.IsUInt32Type) ? "%u" :
+                    (targetMethod.ReturnType.IsInt32Type) ? "%d" :
+                    (targetMethod.ReturnType.IsUInt64Type) ? "%\" PRIu64 \"" :
+                    (targetMethod.ReturnType.IsInt64Type) ? "%\" PRId64 \"" :
+                    (targetMethod.ReturnType.IsSingleType) ? "%f" :
+                    (targetMethod.ReturnType.IsSingleType) ? "%f" :
+                    (targetMethod.ReturnType.IsDoubleType) ? "%lf" :
+                    "%s";
 
-            var result = await GccDriver.CompileAndRunAsync(new StringReader(sourceCode), il2cIncludePath);
-            var lines = result.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+                tw.WriteLine();
+                tw.WriteLine("#include <stdio.h>");
+                tw.WriteLine();
+                tw.WriteLine("int main()");
+                tw.WriteLine("{");
+                tw.WriteLine(string.Format("  auto result = {0}();", targetMethod.CLanguageFunctionName));
+                tw.WriteLine(string.Format("  printf(\"{0}\", result);", formatChar));
+                tw.WriteLine(string.Format("  return 0;"));
+                tw.WriteLine("}");
 
-            Assert.AreEqual(expected?.ToString(), lines[0]);
+                tw.Flush();
+
+                ms.Position = 0;
+                var tr = new StreamReader(ms);
+
+                var result = await GccDriver.CompileAndRunAsync(tr, il2cIncludePath);
+                var lines = result.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+
+                Assert.AreEqual(expected?.ToString(), lines[0]);
+            }
+        }
+
+        public static Task ExecuteTestAsync(Type targetType, string methodName)
+        {
+            var method = targetType.GetMethod(methodName, BindingFlags.Public | BindingFlags.Static);
+            return ExecuteTestAsync(method);
+        }
+
+        public static Task ExecuteTestAsync<T>(Expression<Func<T>> testFunction)
+        {
+            var body = (MethodCallExpression)testFunction.Body;
+            var method = body.Method;
+
+            return ExecuteTestAsync(method);
         }
     }
 }

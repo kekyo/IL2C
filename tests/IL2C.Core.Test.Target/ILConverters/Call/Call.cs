09299ea414eb5afdc0ba2df882e5e69737a76b25
Kouji Matsui <k@kekyo.net>
2018-10-18 11:57:19 +0900

    Add call for instance method tests (WIP)

--- tests/IL2C.Core.Test.Target/ILConverters/Call/Call.cs
+++ tests/IL2C.Core.Test.Target/ILConverters/Call/Call.cs
@@ -6,8 +6,10 @@ namespace IL2C.ILConverters
     [Case(123, new[] { "Static_Int32_Int32", "Add_Static_Int32_Int32" }, 100, 23)]
     [Case("ABCDEF", new[] { "Static_String_String", "Concat_Static_String_String" }, "ABC", "DEF")]
     [Case(123, new[] { "Static_RefInt_RefInt", "Store_Static_RefInt_RefInt" })]
+    //[Case(123, new[] { "Instance_Int32_Int32", "Add_Instance_Int32_Int32" }, 100, 23)]
     public sealed class Call
     {
+        #region Static
         private static int Add_Static_Int32_Int32(int lhs, int rhs)
         {
             return lhs + rhs;
@@ -32,5 +34,16 @@ namespace IL2C.ILConverters
 
         [MethodImpl(MethodImplOptions.ForwardRef)]
         public static extern int Static_RefInt_RefInt();
+        #endregion
+
+        #region Instance
+        private int Add_Instance_Int32_Int32(int lhs, int rhs)
+        {
+            return lhs + rhs;
+        }
+
+        [MethodImpl(MethodImplOptions.ForwardRef)]
+        public static extern int Instance_Int32_Int32(int lhs, int rhs);
+        #endregion
     }
 }

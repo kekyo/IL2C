7fbe2570aaa99beb96316b01e2e8ff535ae0623e
Kouji Matsui <k@kekyo.net>
2018-10-19 16:00:43 +0900

    Bit changed.

--- tests/IL2C.Core.Test.Fixture/Templates/make.bat
+++ tests/IL2C.Core.Test.Fixture/Templates/make.bat
@@ -1,6 +1,6 @@
 ﻿@echo off
 setlocal
 set PATH={gccBinPath};%PATH%
-rmdir /s /q obj
-mkdir obj
-gcc.exe -I D:\PROJECT\IL2C\IL2C.Runtime -save-temps=obj -O0 -g -fdata-sections -ffunction-sections -Wl,--gc-sections -o obj\test.exe test.c
+rmdir /s /q out
+mkdir out
+gcc.exe -I D:\PROJECT\IL2C\IL2C.Runtime -save-temps=obj -O0 -g -fdata-sections -ffunction-sections -Wl,--gc-sections -o out\test.exe test.c

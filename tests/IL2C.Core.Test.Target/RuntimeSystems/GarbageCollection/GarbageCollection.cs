1df42c35ffdba9e2698153519d6b975cde82b1a9
Kouji Matsui <k@kekyo.net>
2018-12-15 15:15:03 +0900

    Added more gc test #12

--- tests/IL2C.Core.Test.Target/RuntimeSystems/GarbageCollection/GarbageCollection.cs
+++ tests/IL2C.Core.Test.Target/RuntimeSystems/GarbageCollection/GarbageCollection.cs
@@ -36,11 +36,28 @@ namespace IL2C.RuntimeSystems
             this.Value = new ObjRefInsideObjRefType(value);
     }
 
+    public struct MultipleInsideValueTypeType
+    {
+        public string Value1;
+        public ObjRefInsideValueTypeType Value2;
+        public ObjRefInsideObjRefType Value3;
+
+        public MultipleInsideValueTypeType(string value1, string value2, string value3)
+        {
+            this.Value1 = value1;
+            this.Value2 = new ObjRefInsideValueTypeType(value2);
+            this.Value3 = new ObjRefInsideObjRefType(value3);
+        }
+    }
+
     [Description("These tests are verified the IL2C manages tracing the object references and collect garbages from the heap memory.")]
     [TestCase("ABCDEF", "ObjRefInsideObjRef", IncludeTypes = new[] { typeof(ObjRefInsideObjRefType) })]
     [TestCase("ABCDEF", "ObjRefInsideValueType", IncludeTypes = new[] { typeof(ObjRefInsideValueTypeType) })]
     [TestCase("ABCDEF", "ObjRefInsideValueTypeInsideObjRef", IncludeTypes = new[] { typeof(ObjRefInsideValueTypeInsideObjRefType), typeof(ObjRefInsideValueTypeType) })]
     [TestCase("ABCDEF", "ObjRefInsideObjRefInsideValueType", IncludeTypes = new[] { typeof(ObjRefInsideObjRefInsideValueTypeType), typeof(ObjRefInsideObjRefType) })]
+    [TestCase("ABCDEF1", "MultipleInsideValueType", 0, IncludeTypes = new[] { typeof(MultipleInsideValueTypeType), typeof(ObjRefInsideValueTypeType), typeof(ObjRefInsideObjRefType) })]
+    [TestCase("ABCDEF2", "MultipleInsideValueType", 1, IncludeTypes = new[] { typeof(MultipleInsideValueTypeType), typeof(ObjRefInsideValueTypeType), typeof(ObjRefInsideObjRefType) })]
+    [TestCase("ABCDEF3", "MultipleInsideValueType", 2, IncludeTypes = new[] { typeof(MultipleInsideValueTypeType), typeof(ObjRefInsideValueTypeType), typeof(ObjRefInsideObjRefType) })]
     public sealed class GarbageCollection
     {
         [MethodImpl(MethodImplOptions.ForwardRef)]
@@ -54,5 +71,8 @@ namespace IL2C.RuntimeSystems
 
         [MethodImpl(MethodImplOptions.ForwardRef)]
         public static extern string ObjRefInsideObjRefInsideValueType();
+
+        [MethodImpl(MethodImplOptions.ForwardRef)]
+        public static extern string MultipleInsideValueType(int index);
     }
 }

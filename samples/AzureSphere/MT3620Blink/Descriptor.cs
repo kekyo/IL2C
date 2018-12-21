edbf074530eb2ac15be6b1ba96fe5a48fa7bf2a5
Kouji Matsui <k@kekyo.net>
2018-12-21 16:41:11 +0900

    Switched command line argument parser to the Mono.Options.

---
+++ samples/AzureSphere/MT3620Blink/Descriptor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MT3620Blink
+{
+    internal sealed class Descriptor : IDisposable
+    {
+        public Descriptor(int fd)
+        {
+            this.Identity = fd;
+        }
+
+        public void Dispose()
+        {
+            if (this.Identity >= 0)
+            {
+                Interops.close(this.Identity);
+                this.Identity = -1;
+            }
+        }
+
+        public int Identity { get; private set; }
+    }
+}

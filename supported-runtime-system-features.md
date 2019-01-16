56b25c51f97b9fbc9320d9aa0d0d118d27e7edae
Kouji Matsui <k@kekyo.net>
2019-01-16 16:17:58 +0900

    Added more event member test.

--- supported-runtime-system-features.md
+++ supported-runtime-system-features.md
@@ -4,7 +4,7 @@
 Feature | Test | Descrition
 |:---|:---|:---|
 | ArrayTypes | [Test [43]](tests/IL2C.Core.Test.Target/RuntimeSystems/ArrayTypes) | Array type generates on the runtime. Example: System.Int32[] isn't declared anything assembly. These tests are verified the IL2C can handle runtime array types and initialize array items from resources. |
-| DelegateTypes | [Test [14]](tests/IL2C.Core.Test.Target/RuntimeSystems/DelegateTypes) | The delegate types contain special member fields, the objref instance reference and the raw method pointer. These tests are verified the IL2C can invoke delegate types combination features between static, instance, virtual method and multicasting at the runtime. |
+| DelegateTypes | [Test [15]](tests/IL2C.Core.Test.Target/RuntimeSystems/DelegateTypes) | The delegate types contain special member fields, the objref instance reference and the raw method pointer. These tests are verified the IL2C can invoke delegate types combination features between static, instance, virtual method and multicasting at the runtime. |
 | EnumTypes | [Test [18]](tests/IL2C.Core.Test.Target/RuntimeSystems/EnumTypes) | The enum types have the underlying primitive integer types. These tests are verified the IL2C can manage enum underlying types at the boxing-unboxing conversion with narrowing-widing combinations. |
 | ExceptionHandling | [Test [31]](tests/IL2C.Core.Test.Target/RuntimeSystems/ExceptionHandling) | These tests are verified the IL2C translates with simple and/or complext exception handling. |
 | ExceptionThrownByCLI | [Test [33]](tests/IL2C.Core.Test.Target/RuntimeSystems/ExceptionThrownByCLI) | These tests are verified the IL2C throw some exceptions from the internal runtimes by ECMA-335 I.12.4.2.1. |
--- tests/IL2C.Core.Test.Target/RuntimeSystems/DelegateTypes/CSharpEvent.cs
+++ tests/IL2C.Core.Test.Target/RuntimeSystems/DelegateTypes/CSharpEvent.cs
@@ -33,14 +33,15 @@
     }
 
     // It's hacked special cases for the C# Roslyn compiler.
+    // The C# event member will contain Interlocked.CompareExchange<T>() generic method.
+    // It can't translate naturally in current version's IL2C.
+    // So it handles the case with hacky code at IL2C.ILConverters.CallConverterUtilities.
+    // This test case validates it.
     [TestId("DelegateTypes")]
     [TestCase(6123, "AddCSharpEvent", 1000, IncludeTypes = new[] { typeof(CSharpEventImpl), typeof(CSharpEventDelegate), typeof(CSharpEventReceiver) })]
+    [TestCase(4123, "RemoveCSharpEvent", 1000, IncludeTypes = new[] { typeof(CSharpEventImpl), typeof(CSharpEventDelegate), typeof(CSharpEventReceiver) })]
     public sealed class CSharpEvent
     {
-        // The C# event member will contain Interlocked.CompareExchange<T>() generic method.
-        // It can't translate naturally in current version's IL2C.
-        // So it handles the case with hacky code at IL2C.ILConverters.CallConverterUtilities.
-        // This test case validates it.
         public static int AddCSharpEvent(int value)
         {
             var receiver = new CSharpEventReceiver();
@@ -50,6 +51,22 @@
             instance.TestEvent += receiver.Receiver1;
             instance.TestEvent += receiver.Receiver2;
             instance.TestEvent += receiver.Receiver3;
+
+            instance.DoEvent(value);
+
+            return receiver.Value;
+        }
+
+        public static int RemoveCSharpEvent(int value)
+        {
+            var receiver = new CSharpEventReceiver();
+            receiver.Value = 123;
+
+            var instance = new CSharpEventImpl();
+            instance.TestEvent += receiver.Receiver1;
+            instance.TestEvent += receiver.Receiver2;
+            instance.TestEvent += receiver.Receiver3;
+            instance.TestEvent -= receiver.Receiver2;
 
             instance.DoEvent(value);
 

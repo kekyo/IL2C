cbd6d671256e5bbbfe93c8f17db81cc73695e8cf
Kouji Matsui <k@kekyo.net>
2019-03-28 07:00:25 +0900

    Omitted ffloat-store option.

--- tests/IL2C.Core.Test.Fixture/GccDriver.cs
+++ tests/IL2C.Core.Test.Fixture/GccDriver.cs
@@ -263,7 +263,7 @@ namespace IL2C
             var outPath = Path.Combine(basePath, "out");
             var executablePath = Path.Combine(outPath, Path.GetFileNameWithoutExtension(sourcePath) + ".exe");
             var libPath = Path.GetFullPath(Path.Combine(gccBasePath, ".."));
-            var optimizeFlag = optimize ? "-Ofast -ffloat-store -DNDEBUG" : "-O0 -ffloat-store -D_DEBUG -DIL2C_USE_RUNTIME_DEBUG_LOG";
+            var optimizeFlag = optimize ? "-Ofast -DNDEBUG" : "-O0 -D_DEBUG -DIL2C_USE_RUNTIME_DEBUG_LOG";
             var disableObjDump = optimize ? "rem " : string.Empty;
 
             if (!Directory.Exists(outPath))

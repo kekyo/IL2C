84c7523f661282596fb1c6eb6dd0b7136fbd2ecd
Kouji Matsui <k@kekyo.net>
2019-05-08 19:44:04 +0900

    Added generic field test. #53

---
+++ tests/IL2C.Core.Test.RuntimeSystems/GenericTypes/GenericTypes.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace IL2C.RuntimeSystems
+{
+    public sealed class ContainsGenericFieldClassType<T>
+    {
+        public T Value;
+    }
+
+    [Description("These tests are verified the IL2C can handle closed generic types.")]
+    [TestCase(12345678, "Int32FieldClassType", 12345678, IncludeTypes =new[] { typeof(ContainsGenericFieldClassType<int>) })]
+    public sealed class GenericTypes
+    {
+        public static int Int32FieldClassType(int value)
+        {
+            var target = new ContainsGenericFieldClassType<int>();
+            target.Value = value;
+            return target.Value;
+        }
+    }
+}

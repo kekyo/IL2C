bde6b99e8ee30ae25500fbb9595cc8fb405fabcf
Kouji Matsui <k@kekyo.net>
2019-05-16 00:22:08 +0900

    Added ArtifactCollector.

---
+++ ArtifactCollector/Executor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IL2C.ArtifactCollector
+{
+    internal static class Executor
+    {
+        public static async Task<Tuple<int, string>> ExecuteAsync(
+            string workingPath, string[] searchPaths, string executablePath, params object[] args)
+        {
+            using (var p = new Process())
+            {
+                p.StartInfo.FileName = executablePath;
+                p.StartInfo.Arguments = string.Join(" ", args);
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.CreateNoWindow = true;
+                p.StartInfo.ErrorDialog = false;
+                p.StartInfo.WorkingDirectory = workingPath;
+
+                if (searchPaths.Length >= 1)
+                {
+                    var pathEnv = p.StartInfo.EnvironmentVariables["PATH"];
+                    p.StartInfo.EnvironmentVariables["PATH"] = string.Join(";", searchPaths) + ";" + pathEnv;
+                }
+
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.RedirectStandardError = true;
+
+                var sb = new StringBuilder();
+                p.OutputDataReceived += (sender, e) => { if (e.Data != null) lock (sb) sb.AppendLine(e.Data); };
+                p.ErrorDataReceived += (sender, e) => { if (e.Data != null) lock (sb) sb.AppendLine(e.Data); };
+
+                var tcs = new TaskCompletionSource<int>();
+                p.Exited += (sender, e) => tcs.SetResult(p.ExitCode);
+                p.EnableRaisingEvents = true;
+
+                p.Start();
+
+                p.BeginOutputReadLine();
+                p.BeginErrorReadLine();
+
+                var exitCode = await tcs.Task;
+
+                p.WaitForExit();
+
+                p.CancelOutputRead();
+                p.CancelErrorRead();
+
+                return Tuple.Create(exitCode, sb.ToString());
+            }
+        }
+    }
+}
--- pack.bat
+++ pack.bat
@@ -1,66 +1,17 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set VERSION=0.4.60
+rem if not exist ArtifactCollector\ArtifactCollector.exe (
+rem     echo ArtifactCollector not found.
+rem     echo Build il2c.sln first.
+rem     exit /b 1
+rem )
 
-if exist artifacts (
-    move artifacts artifacts_
-    rmdir /s /q artifacts_
-)
-mkdir artifacts
+dotnet build --configuration Release il2c.sln
 
-echo.
-echo ///////////////////////////////////////////////
-echo // Build IL2C.Interop
-echo.
+ArtifactCollector\ArtifactCollector.exe . .\artifacts IL2C.Interop IL2C.Core IL2C.Tasks IL2C.Runtime
 
-dotnet pack --configuration Release --include-symbols IL2C.Interop\IL2C.Interop.csproj
-copy IL2C.Interop\bin\Release\IL2C.Interop.%VERSION%.symbols.nupkg artifacts\IL2C.Interop.%VERSION%.nupkg
-
-echo.
-echo ///////////////////////////////////////////////
-echo // Build IL2C.Core
-echo.
-
-dotnet pack --configuration Release --include-symbols IL2C.Core\IL2C.Core.csproj
-copy IL2C.Core\bin\Release\IL2C.Core.%VERSION%.symbols.nupkg artifacts\IL2C.Core.%VERSION%.nupkg
-
-echo.
-echo ///////////////////////////////////////////////
-echo // Build IL2C.Runtime
-echo.
-
-.nuget\nuget pack -Prop version=%VERSION% -OutputDirectory artifacts IL2C.Runtime\IL2C.Runtime.nuspec 
-
-echo.
-echo ///////////////////////////////////////////////
-echo // Build IL2C.Runtime.msvc
-echo.
-
-.nuget\nuget pack -Prop version=%VERSION% -OutputDirectory artifacts IL2C.Runtime\IL2C.Runtime.msvc.nuspec 
-
-echo.
-echo ///////////////////////////////////////////////
-echo // Build IL2C.Build
-echo.
-
-dotnet pack --configuration Release --include-symbols IL2C.Tasks\IL2C.Tasks.csproj
-copy IL2C.Tasks\bin\Release\IL2C.Build.%VERSION%.symbols.nupkg artifacts\IL2C.Build.%VERSION%.nupkg
-
-echo.
-echo ///////////////////////////////////////////////
-echo // Build Arduino library
-echo.
-
-mkdir artifacts\Arduino
-xcopy /s IL2C.Runtime\include\*.h artifacts\Arduino\src\
-xcopy /s IL2C.Runtime\src\*.h artifacts\Arduino\src\
-xcopy /s IL2C.Runtime\src\*.c artifacts\Arduino\src\
-
-for /f "delims=" %%a in (Arduino.properties) do (
-  set line=%%a
-  echo !line:{version}=%VERSION%! >> artifacts\Arduino\library.properties
-)
+rem .nuget\nuget.exe push -Source "IL2C" -ApiKey AzureDevOps .\artifacts\*.nupkg
 
 echo.
 echo Done.

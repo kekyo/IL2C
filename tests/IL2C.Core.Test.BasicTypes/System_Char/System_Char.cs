25045edb6a4bf1deab9b81d0ccd88b8b80b89180
Kouji Matsui <k@kekyo.net>
2020-06-16 21:02:58 +0900

    Fixed System.Char some tests when using invalid null terminated string contained arguments.

--- tests/IL2C.Core.Test.BasicTypes/System_Char/System_Char.cs
+++ tests/IL2C.Core.Test.BasicTypes/System_Char/System_Char.cs
@@ -23,10 +23,10 @@ namespace IL2C.BasicTypes
 {
     [TestCase(true, "IsValueType")]
     [TestCase(2, "SizeOf")]
-    [TestCase("\uffff", "ToString", char.MaxValue)]
-    [TestCase("\u0000", "ToString", char.MinValue)]
-    [TestCase(char.MaxValue, "TryParse", "\uffff")]
-    [TestCase(char.MinValue, "TryParse", "\u0000")]
+    [TestCase("\u0000", "ToStringMin")]
+    [TestCase("\uffff", "ToStringMax")]
+    [TestCase(char.MinValue, "TryParseMin")]
+    [TestCase(char.MaxValue, "TryParseMax")]
     public sealed class System_Char
     {
         [MethodImpl(MethodImplOptions.ForwardRef)]
@@ -35,14 +35,33 @@ namespace IL2C.BasicTypes
         [MethodImpl(MethodImplOptions.ForwardRef)]
         public static extern int SizeOf();
 
-        public static string ToString(char value)
+        ///////////////////////////////
+        // NUnit (or visual studio test engine) problem:
+        //   Will apply unique test case names with overload arguments.
+        //   (ex: ToString("\u0000") and ToString("\uffff") if apply sets for test methods)
+        //   But cause failed conflicting both these test case names (maybe contains invalid char).
+
+        public static string ToStringMin()
         {
+            char value = char.MinValue;
             return value.ToString();
         }
 
-        public static char TryParse(string str)
+        public static string ToStringMax()
         {
-            char.TryParse(str, out var value);
+            char value = char.MaxValue;
+            return value.ToString();
+        }
+
+        public static char TryParseMin()
+        {
+            char.TryParse("\u0000", out var value);
+            return value;
+        }
+
+        public static char TryParseMax()
+        {
+            char.TryParse("\uffff", out var value);
             return value;
         }
     }

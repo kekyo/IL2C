2497486c62ed303337fdda95004a4efc40053357
Kouji Matsui <k@kekyo.net>
2020-06-23 20:25:30 +0900

    Updated GettingStarted samples. #80

--- samples/GettingStartedIL2C/GettingStartedIL2C/Program.cs
+++ samples/GettingStartedIL2C/GettingStartedIL2C/Program.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace GettingStartedIL2C
 {
     public static class Program
     {
-        public static void Main() =>
+        [DllImport("user32.dll")]
+        public static extern int MessageBoxW(
+           IntPtr hWnd, string text, string caption, int options);
+        
+        public static void Main()
+        {
             Console.WriteLine("Hello world with IL2C!");
+            //MessageBoxW(IntPtr.Zero, "Hello world with IL2C!", "Getting started IL2C", 1);
+        }
     }
 }

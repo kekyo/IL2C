27889d9d2db2cc6d36bb4bd4ecfcae3d57122647
Kouji Matsui <k@kekyo.net>
2021-10-09 17:44:08 +0900

    Added more strictly integral promotion test at conv.i. #95 #96

--- tests/IL2C.Core.Test.ILConverters/Conv_i/Conv_i.cs
+++ tests/IL2C.Core.Test.ILConverters/Conv_i/Conv_i.cs
@@ -22,13 +22,26 @@ using System.Runtime.CompilerServices;
 
 namespace IL2C.ILConverters
 {
+    [TestCase(123, "SByte", (sbyte)123)]
+    [TestCase(12345, "Int16", (short)12345)]
     [TestCase(12345, "Int32", 12345)]
     [TestCase(12345, "Int64", 12345L)]
     [TestCase(12345, "IntPtr", 12345)]
+    [TestCase(123, "Byte", (byte)123)]
+    [TestCase(12345, "UInt16", (ushort)12345)]
+    [TestCase(12345, "UInt32", (uint)12345)]
+    [TestCase(12345, "UInt64", 12345UL)]
+    [TestCase(12345, "UIntPtr", (uint)12345)]
     [TestCase(12345, "Single", 12345.67f)]
     [TestCase(12345, "Double", 12345.67)]
     public sealed class Conv_i
     {
+        [MethodImpl(MethodImplOptions.ForwardRef)]
+        public static extern IntPtr SByte(sbyte value);
+
+        [MethodImpl(MethodImplOptions.ForwardRef)]
+        public static extern IntPtr Int16(short value);
+
         [MethodImpl(MethodImplOptions.ForwardRef)]
         public static extern IntPtr Int32(int value);
 
@@ -37,6 +50,21 @@ namespace IL2C.ILConverters
 
         [MethodImpl(MethodImplOptions.ForwardRef)]
         public static extern IntPtr IntPtr(IntPtr value);
+
+        [MethodImpl(MethodImplOptions.ForwardRef)]
+        public static extern IntPtr Byte(byte value);
+
+        [MethodImpl(MethodImplOptions.ForwardRef)]
+        public static extern IntPtr UInt16(ushort value);
+
+        [MethodImpl(MethodImplOptions.ForwardRef)]
+        public static extern IntPtr UInt32(uint value);
+
+        [MethodImpl(MethodImplOptions.ForwardRef)]
+        public static extern IntPtr UInt64(ulong value);
+
+        [MethodImpl(MethodImplOptions.ForwardRef)]
+        public static extern IntPtr UIntPtr(UIntPtr value);
 
         [MethodImpl(MethodImplOptions.ForwardRef)]
         public static extern IntPtr Single(float value);

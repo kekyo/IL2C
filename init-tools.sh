add7e868560d49f1e00db55a4e145a571a44d4a0
Kouji Matsui <k@kekyo.net>
2021-10-21 20:02:05 +0900

    Added mono-devel on init-tools.sh

--- init-tools.sh
+++ init-tools.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+# Required: .NET 5.0 SDK
+#   https://dotnet.microsoft.com/download/dotnet/5.0
+
 echo "Setup native binary toolchain ..."
 
-apt-get update -y
-apt-get install build-essential cmake ninja-build -y
+apt update -y
+apt install build-essential cmake ninja-build mono-devel -y
+
+# `mono-devel` is required only running regression test. (net48 platform)
 
 chmod 755 *.sh

78f0f2e53c20fcd4425035cfc88d8226ff2c0058
Kouji Matsui <k@kekyo.net>
2022-05-03 22:23:29 +0900

    Implemented new building infrastructure by cli packaged.

--- src/IL2C.Build/Program.cs
+++ src/IL2C.Build/Program.cs
@@ -29,22 +29,26 @@ namespace IL2C
                 var enableCpp = false;
                 var enableBundler = false;
                 var targetPlatform = TargetPlatforms.Generic;
+                var refs = new string[0];
                 var help = false;
 
                 var options = new OptionSet()
                 {
-                    { "g1|debug", "Emit debug informations (contains only comments)", v => debugInformationOptions = DebugInformationOptions.CommentOnly },
-                    { "g|g2|debug-full", "Emit debug informations (contains line numbers)", v => debugInformationOptions = DebugInformationOptions.Full },
-                    { "no-read-symbols", "NO read symbol files", _ => readSymbols = false },
-                    { "cpp", "Produce C++ extension files (apply extension *.cpp instead *.c, body will not change)", _ => enableCpp = true },
-                    { "bundler", "Produce bundler source file", _ => enableBundler = true },
+                    { "debug=", "Emit debug informations [none|commentonly|full]", v => debugInformationOptions = Enum.TryParse<DebugInformationOptions>(v, true, out var t) ? t : DebugInformationOptions.None },
+                    { "readSymbol=", "Read symbol files [true|false]", v => readSymbols = bool.TryParse(v, out var rs) ? rs : true },
+                    { "cpp=", "Produce C++ extension files (apply extension *.cpp instead *.c, body will not change)", v => enableCpp = bool.TryParse(v, out var ec) ? ec : false },
+                    { "bundler=", "Produce bundler source file", _ => enableBundler = true },
                     { "target=", "Target platform [generic|ue4]", v => targetPlatform = Enum.TryParse<TargetPlatforms>(v, true, out var t) ? t : TargetPlatforms.Generic },
+                    { "refs=", "Reference assembly paths (semi-colon separated)", v => refs = v.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries) },
                     { "h|help", "Print this help", _ => help = true },
                 };
 
                 var extra = options.Parse(args);
                 if (help || (extra.Count < 2))
                 {
+                    Console.Out.WriteLine($"IL2C.Build [{ThisAssembly.AssemblyVersion}]");
+                    Console.Out.WriteLine("  A translator for ECMA-335 CIL/MSIL to C language.");
+                    Console.Out.WriteLine("  Copyright (c) Kouji Matsui.");
                     Console.Out.WriteLine("usage: il2c.exe [options] <output_path> <assembly_path>");
                     options.WriteOptionDescriptions(Console.Out);
                 }
@@ -52,6 +56,8 @@ namespace IL2C
                 {
                     var outputPath = extra[0];
                     var assemblyPaths = extra.Skip(1);
+
+                    // TODO: refs
 
                     SimpleDriver.TranslateAll(
                         Console.Out,

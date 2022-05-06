75181613aa56d681636b20f9712c6f19ec3d48fb
Kouji Matsui <k@kekyo.net>
2022-05-06 21:49:19 +0900

    Added mingw32 toolchain binary.

--- misc/IL2C.toolchain.gcc4.mingw32/fetch.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-curl -L -O https://github.com/kekyo/IL2C/releases/download/IL2C-toolchain-gcc4-mingw32-4/IL2C-toolchain-gcc4-mingw32-4-base.exe
-if errorlevel 1 (
-    exit /b %errorlevel%
-)

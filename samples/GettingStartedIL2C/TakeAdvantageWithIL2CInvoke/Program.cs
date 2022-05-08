2662ae6b726608cf65b435c4f89d6a84e960760a
Kouji Matsui <k@kekyo.net>
2022-05-08 21:54:14 +0900

    Added play ground projects.

---
+++ samples/GettingStartedIL2C/TakeAdvantageWithIL2CInvoke/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace TakeAdvantageWithIL2CInvoke
+{
+    // Refer timespec struct type from pure C header file.
+    [NativeType("time.h", SymbolName = "struct timespec")]
+    internal struct timespec
+    {
+        public int tv_sec;
+        public int tv_nsec;
+    }
+
+    public static class Program
+    {
+        // It is intruduced what/how to do 'IL2C/Invoke'.
+        // Explained blog entry: https://www.kekyo.net/2019/01/04/6917
+
+        // Refer nanosleep() API from pure C header file.
+        [NativeMethod("time.h")]
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private static extern void nanosleep(
+            ref timespec time, ref timespec remains);
+
+        public static void Main()
+        {
+            var sleepTime = new timespec { tv_sec = 1 };
+            var dummy = new timespec();
+
+            while (true)
+            {
+                Console.WriteLine("Hmm...");
+                nanosleep(ref sleepTime, ref dummy);
+            }
+        }
+    }
+}
---
+++ src/PlayGround/ShowMessageBoxOnWindows/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace GettingStartedIL2C
+{
+    public static class Program
+    {
+        public enum MessageBoxOptions : uint
+        {
+            MB_OK = 0,
+            MB_OKCANCEL,
+            MB_ABORTRETRYIGNORE,
+            MB_YESNOCANCEL,
+            MB_YESNO,
+            MB_RETRYCANCEL,
+            MB_CANCELTRYCONTINUE,
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern int MessageBoxW(
+           IntPtr hWnd, string text, string caption, MessageBoxOptions options);
+
+        public static int Main() => 
+            MessageBoxW(
+                IntPtr.Zero,
+                "Hello world with IL2C!", "Getting started IL2C",
+                MessageBoxOptions.MB_YESNOCANCEL);
+    }
+}

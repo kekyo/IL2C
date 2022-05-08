c3965f1e74d9831efdd279597c5ade89eca8d4e3
Kouji Matsui <k@kekyo.net>
2022-05-08 20:03:45 +0900

    Updated samples.

---
+++ samples/GettingStartedIL2C/HelloWorld/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public static class Program
+    {
+        public static void Main() =>
+            Console.WriteLine("Hello world with IL2C!");
+    }
+}

269470f50e907c4ae3da3a6d404d9a1b4db6c716
Kouji Matsui <k@kekyo.net>
2022-05-10 09:36:31 +0900

    Changed init-tools.sh installation by script based sdk.

--- init-tools.sh
+++ init-tools.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
 
-# Required: .NET 6.0 SDK
-#   https://dotnet.microsoft.com/en-us/download/dotnet/6.0
+# `mono-devel` is required only running regression test. (net48 platform)
 
 echo "Setup native binary toolchain ..."
 
-apt update -y
-apt install build-essential cmake ninja-build mono-devel -y
+sudo apt update -y
+sudo apt install build-essential cmake ninja-build mono-devel -y
 
-# `mono-devel` is required only running regression test. (net48 platform)
+#curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin -c 2.1
+#curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin -c 2.2
+#curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin -c 3.1
+#curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin -c 5.0
+#curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin -c 6.0
 
-chmod 755 *.sh
+#export PATH=$HOME/.dotnet:$PATH
+#export DOTNET_HOME=$HOME/.dotnet
+
+#echo "You have to set environment variables in your sh profile:"
+
+#echo "export PATH=$HOME/.dotnet:$PATH"
+#echo "export DOTNET_HOME=$HOME/.dotnet"
+

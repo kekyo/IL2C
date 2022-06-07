d7d75940b55e54cf006e95bc0f94dd6a86edecc1
Kouji Matsui <k@kekyo.net>
2022-06-07 09:17:48 +0900

    Fixed ldarg.2 opcode tests with IntPtr type.

--- tests/IL2C.Core.Test.ILConverters/Ldarg_2/Ldarg_2.cs
+++ tests/IL2C.Core.Test.ILConverters/Ldarg_2/Ldarg_2.cs
@@ -51,13 +51,19 @@ namespace IL2C.ILConverters
         [MethodImpl(MethodImplOptions.ForwardRef)]
         public static extern ulong UInt64(int arg0, int arg1, ulong num);
 
+            [MethodImpl(MethodImplOptions.ForwardRef)]
+            private static extern IntPtr IntPtrImpl(int arg0, int arg1, IntPtr num);
+
         [TestCase(int.MaxValue - 1, "IntPtr", 123, 45, int.MaxValue)]
-        [MethodImpl(MethodImplOptions.ForwardRef)]
-        public static extern IntPtr IntPtr(int arg0, int arg1, IntPtr num);
+        public static int IntPtr(int arg0, int arg1, int num) =>
+            IntPtrImpl(arg0, arg1, (IntPtr)num).ToInt32();
+
+            [MethodImpl(MethodImplOptions.ForwardRef)]
+            private static extern UIntPtr UIntPtrImpl(int arg0, int arg1, UIntPtr num);
 
         [TestCase(uint.MaxValue - 1, "UIntPtr", 123, 45, uint.MaxValue)]
-        [MethodImpl(MethodImplOptions.ForwardRef)]
-        public static extern UIntPtr UIntPtr(int arg0, int arg1, UIntPtr num);
+        public static uint UIntPtr(int arg0, int arg1, uint num) =>
+            UIntPtrImpl(arg0, arg1, (UIntPtr)num).ToUInt32();
 
         [TestCase((float)((double)123.45f + (double)3.14159274f), "Single", 123, 45, 123.45f)]
         [MethodImpl(MethodImplOptions.ForwardRef)]

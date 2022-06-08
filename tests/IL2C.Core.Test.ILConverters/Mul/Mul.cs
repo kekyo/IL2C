ae9eecb5bc7b190159e96542fee155de796da62c
Kouji Matsui <k@kekyo.net>
2022-06-08 23:01:24 +0900

    Fixed mul opcode tests with IntPtr type.

--- tests/IL2C.Core.Test.ILConverters/Mul/Mul.cs
+++ tests/IL2C.Core.Test.ILConverters/Mul/Mul.cs
@@ -18,21 +18,30 @@ namespace IL2C.ILConverters
         [MethodImpl(MethodImplOptions.ForwardRef)]
         public static extern int Int32_Int32(int lhs, int rhs);
 
+            [MethodImpl(MethodImplOptions.ForwardRef)]
+            private static extern IntPtr Int32_IntPtrImpl(int lhs, IntPtr rhs);
+
         [TestCase(288, "Int32_IntPtr", 12, 24)]
-        [MethodImpl(MethodImplOptions.ForwardRef)]
-        public static extern IntPtr Int32_IntPtr(int lhs, IntPtr rhs);
+        public static int Int32_IntPtr(int lhs, int rhs) =>
+            Int32_IntPtrImpl(lhs, (IntPtr)rhs).ToInt32();
 
         [TestCase(288L, "Int64_Int64", 12L, 24L)]
         [MethodImpl(MethodImplOptions.ForwardRef)]
         public static extern long Int64_Int64(long lhs, long rhs);
 
+            [MethodImpl(MethodImplOptions.ForwardRef)]
+            private static extern IntPtr IntPtr_Int32Impl(IntPtr lhs, int rhs);
+
         [TestCase(288, "IntPtr_Int32", 12, 24)]
-        [MethodImpl(MethodImplOptions.ForwardRef)]
-        public static extern IntPtr IntPtr_Int32(IntPtr lhs, int rhs);
+        public static int IntPtr_Int32(int lhs, int rhs) =>
+            IntPtr_Int32Impl((IntPtr)lhs, rhs).ToInt32();
+
+            [MethodImpl(MethodImplOptions.ForwardRef)]
+            private static extern IntPtr IntPtr_IntPtrImpl(IntPtr lhs, IntPtr rhs);
 
         [TestCase(288, "IntPtr_IntPtr", 12, 24)]
-        [MethodImpl(MethodImplOptions.ForwardRef)]
-        public static extern IntPtr IntPtr_IntPtr(IntPtr lhs, IntPtr rhs);
+        public static int IntPtr_IntPtr(int lhs, int rhs) =>
+            IntPtr_IntPtrImpl((IntPtr)lhs, (IntPtr)rhs).ToInt32();
 
         [TestCase(123.0f * 0.45f, "Single_Single", 123.0f, 0.45f)]
         [MethodImpl(MethodImplOptions.ForwardRef)]

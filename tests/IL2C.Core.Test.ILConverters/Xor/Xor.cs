540d46f52a46b104496468a6eb6c407422e4f33e
Kouji Matsui <k@kekyo.net>
2022-06-08 23:17:28 +0900

    Fixed xor opcode tests with IntPtr type.

--- tests/IL2C.Core.Test.ILConverters/Xor/Xor.cs
+++ tests/IL2C.Core.Test.ILConverters/Xor/Xor.cs
@@ -18,20 +18,29 @@ namespace IL2C.ILConverters
         [MethodImpl(MethodImplOptions.ForwardRef)]
         public static extern int Int32_Int32(int lhs, int rhs);
 
+            [MethodImpl(MethodImplOptions.ForwardRef)]
+            private static extern IntPtr Int32_IntPtrImpl(int lhs, IntPtr rhs);
+
         [TestCase(0x12345678 ^ 0x55555555, "Int32_IntPtr", 0x12345678, 0x55555555)]
-        [MethodImpl(MethodImplOptions.ForwardRef)]
-        public static extern IntPtr Int32_IntPtr(int lhs, IntPtr rhs);
+        public static int Int32_IntPtr(int lhs, int rhs) =>
+            Int32_IntPtrImpl(lhs, (IntPtr)rhs).ToInt32();
 
         [TestCase(0x123456789abcdef ^ 0x55555555aaaaaaaa, "Int64_Int64", 0x123456789abcdef, 0x55555555aaaaaaaa)]
         [MethodImpl(MethodImplOptions.ForwardRef)]
         public static extern long Int64_Int64(long lhs, long rhs);
 
+            [MethodImpl(MethodImplOptions.ForwardRef)]
+            private static extern IntPtr IntPtr_Int32Impl(IntPtr lhs, int rhs);
+
         [TestCase(0x12345678 ^ 0x55555555, "IntPtr_Int32", 0x12345678, 0x55555555)]
-        [MethodImpl(MethodImplOptions.ForwardRef)]
-        public static extern IntPtr IntPtr_Int32(IntPtr lhs, int rhs);
+        public static int IntPtr_Int32(int lhs, int rhs) =>
+            IntPtr_Int32Impl((IntPtr)lhs, rhs).ToInt32();
+
+            [MethodImpl(MethodImplOptions.ForwardRef)]
+            private static extern IntPtr IntPtr_IntPtrImpl(IntPtr lhs, IntPtr rhs);
 
         [TestCase(0x12345678 ^ 0x55555555, "IntPtr_IntPtr", 0x12345678, 0x55555555)]
-        [MethodImpl(MethodImplOptions.ForwardRef)]
-        public static extern IntPtr IntPtr_IntPtr(IntPtr lhs, IntPtr rhs);
+        public static int IntPtr_IntPtr(int lhs, int rhs) =>
+            IntPtr_IntPtrImpl((IntPtr)lhs, (IntPtr)rhs).ToInt32();
     }
 }
